98707127a82844da92f1a85ab9d9902bf7c892c0
M. A. F. Bento <miguel.angelo.f.bento@gmail.com>
2021-04-30 23:50:46 +0200

    Add router

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,32 @@
 <template>
   <div id="app">
-    <LanguageSelector />
-    <HelloWorld :msg="$t('hello')" />
-    <img class="logo" alt="Vue logo" src="./assets/world.svg" />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import LanguageSelector from "./components/LanguageSelector.vue";
-import i18n from "./plugin/i18n";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-    LanguageSelector,
-  },
-  beforeCreate() {
-    if (localStorage.locale) {
-      i18n.locale = localStorage.locale;
-    }
-  },
-};
-</script>
-
 <style lang="scss">
-@import "~sanitize.css";
-@import "~sanitize.css/typography.css";
-
-$defaultSpace: 3rem;
-
 #app {
-  font-family: Titillium Web, Helvetica, Arial, sans-serif;
-  font-size: var(--font-size);
-  line-height: 1.4;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  background-color: var(--dark);
-  color: var(--light);
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  padding-top: $defaultSpace;
-  padding-bottom: $defaultSpace;
+  color: #2c3e50;
 }
 
-.logo {
-  margin-top: $defaultSpace;
-  padding: $defaultSpace;
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
